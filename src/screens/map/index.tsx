2b8a0d26451db2aab75c542c6f426fd1125486fb
Denis Toledo <contato@denistoledo.com.br>
2024-03-05 08:21:52 +0100

    feature: map first integration

--- src/screens/map/index.tsx
+++ src/screens/map/index.tsx
@@ -1,9 +1,151 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Dimensions, Animated } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Avatar, Card } from 'react-native-paper';
+
+import { data } from './mock';
+
+const { width } = Dimensions.get('window');
+const CARD_WIDTH = width / 2;
 
 export default function Map() {
+  const animation = new Animated.Value(0);
+
+  const interpolations = data.state.markers.map((marker, index) => {
+    const inputRange = [
+      (index - 1) * CARD_WIDTH,
+      index * CARD_WIDTH,
+      (index + 1) * CARD_WIDTH,
+    ];
+    const scale = animation.interpolate({
+      inputRange,
+      outputRange: [1, 2.5, 1],
+      extrapolate: 'clamp',
+    });
+    const opacity = animation.interpolate({
+      inputRange,
+      outputRange: [0.35, 1, 0.35],
+      extrapolate: 'clamp',
+    });
+    return { scale, opacity };
+  });
+
+  const onMarkerSelected = (marker: any) => {};
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Map</Text>
+    <View style={{ flex: 1 }}>
+      <MapView
+        initialRegion={data.region}
+        provider={PROVIDER_GOOGLE}
+        style={styles.container}
+        showsUserLocation
+        tintColor="red"
+      >
+        {data.state.markers.map((marker, index) => {
+          const scaleStyle = [
+            {
+              transform: [
+                {
+                  scale: interpolations[index].scale,
+                },
+              ],
+            },
+          ];
+          const opacityStyle = [
+            {
+              opacity: interpolations[index].opacity,
+            },
+          ];
+          return (
+            <Marker
+              key={index}
+              title={marker.title}
+              description={marker.description}
+              coordinate={marker.coordinates}
+              onPress={() => onMarkerSelected(marker)}
+            >
+              <Animated.View style={[styles.markerWrap, opacityStyle]}>
+                <Animated.View style={[styles.ring, scaleStyle]} />
+                <View style={styles.marker} />
+              </Animated.View>
+            </Marker>
+          );
+        })}
+      </MapView>
+      <Animated.ScrollView
+        horizontal
+        scrollEventThrottle={1}
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={CARD_WIDTH}
+        snapToAlignment="center"
+        style={styles.scrollView}
+        contentContainerStyle={styles.endPadding}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: animation,
+                },
+              },
+            },
+          ],
+          { useNativeDriver: true },
+        )}
+      >
+        {data.state.markers.map((marker, index) => (
+          <Card key={index} style={styles.card}>
+            <Card.Title
+              title={marker.title}
+              titleVariant="titleSmall"
+              subtitle={marker.description}
+              subtitleVariant="bodySmall"
+              left={(props) => (
+                <Avatar.Image size={42} source={marker.image!} />
+              )}
+            />
+          </Card>
+        ))}
+      </Animated.ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    position: 'absolute',
+    bottom: 30,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+  endPadding: {
+    paddingRight: width - CARD_WIDTH,
+  },
+  card: {
+    marginHorizontal: 8,
+    width: CARD_WIDTH,
+  },
+  markerWrap: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marker: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: 'rgba(0,100,0, 1)',
+    zIndex: 1,
+  },
+  ring: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'rgba(173,255,47, 0.7)',
+    position: 'absolute',
+    borderWidth: 1,
+    borderColor: 'rgba(173,255,47, 0.5)',
+  },
+});
---
+++ src/screens/map/mock.ts
@@ -0,0 +1,247 @@
+const images = [
+  { uri: 'https://i.imgur.com/sNam9iJ.jpg' },
+  { uri: 'https://i.imgur.com/N7rlQYt.jpg' },
+  { uri: 'https://i.imgur.com/UDrH0wm.jpg' },
+  { uri: 'https://i.imgur.com/Ka8kNST.jpg' },
+];
+
+export const data = {
+  state: {
+    markers: [
+      {
+        coordinates: {
+          latitude: 52.52,
+          longitude: 13.405,
+        },
+        title: 'Green Leaf Lounge',
+        description: 'Experience the best cannabis strains in central Berlin.',
+        image: images[0],
+      },
+      {
+        coordinates: {
+          latitude: 52.4783,
+          longitude: 13.3298,
+        },
+        title: 'Kreuzberg Kush Corner',
+        description:
+          'Find your favorite Kush varieties in the vibrant Kreuzberg district.',
+        image: images[1],
+      },
+      {
+        coordinates: {
+          latitude: 52.53,
+          longitude: 13.385,
+        },
+        title: 'Prenzlauer Heights Dispensary',
+        description:
+          'Elevate your senses with high-quality cannabis in Prenzlauer Berg.',
+        image: images[2],
+      },
+      {
+        coordinates: {
+          latitude: 52.5022,
+          longitude: 13.3949,
+        },
+        title: 'City Green Gardens',
+        description:
+          'Explore a green oasis with diverse cannabis options in Berlin Mitte.',
+        image: images[3],
+      },
+      {
+        coordinates: {
+          latitude: 52.4831,
+          longitude: 13.4136,
+        },
+        title: 'Friedrichshain Fields',
+        description:
+          'Enjoy the relaxing ambiance and premium cannabis in Friedrichshain.',
+        image: images[2],
+      },
+      {
+        coordinates: {
+          latitude: 52.4946,
+          longitude: 13.4305,
+        },
+        title: 'East Side Herbal Hub',
+        description:
+          'Discover unique herbal blends on the vibrant east side of Berlin.',
+        image: images[0],
+      },
+      {
+        coordinates: {
+          latitude: 52.466,
+          longitude: 13.3946,
+        },
+        title: 'Kreuzkölln Cannabis Corner',
+        description:
+          'Blend of Kreuzberg and Neukölln vibes with top-notch cannabis.',
+        image: images[3],
+      },
+      {
+        coordinates: {
+          latitude: 52.5368,
+          longitude: 13.3872,
+        },
+        title: 'Weißensee Weed Wonderland',
+        description:
+          'Step into a wonderland of diverse weed options in Weißensee.',
+        image: images[2],
+      },
+      {
+        coordinates: {
+          latitude: 52.4611,
+          longitude: 13.3054,
+        },
+        title: 'Tempelhof THC Terrace',
+        description:
+          'Relax and unwind at the terrace with a view in Tempelhof.',
+      },
+      {
+        coordinates: {
+          latitude: 52.509,
+          longitude: 13.4395,
+        },
+        title: 'Gleisdreieck Greenhouse',
+        description:
+          'Unique strains await you in the Gleisdreieck urban greenhouse.',
+        image: images[1],
+      },
+      {
+        coordinates: {
+          latitude: 52.5254,
+          longitude: 13.3695,
+        },
+        title: 'Alexanderplatz Cannabis Corner',
+        description:
+          'Explore a diverse selection of cannabis near Alexanderplatz.',
+        image: images[0],
+      },
+      {
+        coordinates: {
+          latitude: 52.4838,
+          longitude: 13.3499,
+        },
+        title: 'Marijuana Mansion in Moabit',
+        description:
+          'Indulge in the luxury of premium marijuana in the heart of Moabit.',
+        image: images[3],
+      },
+      {
+        coordinates: {
+          latitude: 52.488,
+          longitude: 13.378,
+        },
+        title: 'Spree-side Sativa Spot',
+        description: 'Enjoy Sativa strains by the Spree River in Berlin.',
+        image: images[0],
+      },
+      {
+        coordinates: {
+          latitude: 52.5233,
+          longitude: 13.3933,
+        },
+        title: 'Mauerpark Medley',
+        description: 'A medley of cannabis options awaits you at Mauerpark.',
+        image: images[2],
+      },
+      {
+        coordinates: {
+          latitude: 52.4714,
+          longitude: 13.4179,
+        },
+        title: 'Neukölln Nugs Nook',
+        description: 'Find your favorite nugs at the cozy Neukölln Nook.',
+        image: images[3],
+      },
+      {
+        coordinates: {
+          latitude: 52.4966,
+          longitude: 13.4362,
+        },
+        title: 'Warschauer Weed Wharf',
+        description: 'Dock into diverse weed options at the Warschauer Wharf.',
+        image: images[0],
+      },
+      {
+        coordinates: {
+          latitude: 52.5115,
+          longitude: 13.444,
+        },
+        title: 'Tiergarten THC Terrace',
+        description: 'Enjoy a terrace experience with THC near Tiergarten.',
+        image: images[1],
+      },
+      {
+        coordinates: {
+          latitude: 52.5291,
+          longitude: 13.4033,
+        },
+        title: 'Potsdamer Platz Pot Palace',
+        description: 'Discover a palace of pot varieties near Potsdamer Platz.',
+        image: images[0],
+      },
+      {
+        coordinates: {
+          latitude: 52.4796,
+          longitude: 13.3493,
+        },
+        title: 'Kreuzberg Kief Kingdom',
+        description: 'Immerse yourself in the kingdom of kief in Kreuzberg.',
+        image: images[3],
+      },
+      {
+        coordinates: {
+          latitude: 52.52,
+          longitude: 13.3784,
+        },
+        title: 'Mitte Marijuana Marketplace',
+        description:
+          'Explore a marketplace of marijuana options in Berlin Mitte.',
+        image: images[2],
+      },
+      {
+        coordinates: {
+          latitude: 52.5387,
+          longitude: 13.4171,
+        },
+        title: 'Pankow Pot Paradise',
+        description: 'Escape to a pot paradise in the lush greenery of Pankow.',
+        image: images[1],
+      },
+      {
+        coordinates: {
+          latitude: 52.4662,
+          longitude: 13.3233,
+        },
+        title: 'Marienfelde Medicated Meadow',
+        description: 'Relax in the medicated meadow of Marienfelde.',
+        image: images[0],
+      },
+      {
+        coordinates: {
+          latitude: 52.5013,
+          longitude: 13.3938,
+        },
+        title: 'Checkpoint Charlie Cannabis Club',
+        description:
+          'Experience premium cannabis near historic Checkpoint Charlie.',
+        image: images[1],
+      },
+      {
+        coordinates: {
+          latitude: 52.5353,
+          longitude: 13.4205,
+        },
+        title: 'Weißensee Weed Wholesalers',
+        description: 'Wholesale weed options available in Weißensee.',
+        image: images[2],
+      },
+    ],
+  },
+  region: {
+    latitude: 52.520008,
+    longitude: 13.404954,
+    latitudeDelta: 0.3,
+    longitudeDelta: 0.3,
+  },
+};
